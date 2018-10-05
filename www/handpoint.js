9401f277fc53ba6bcbc9acc674919bfbfbaec668
MobileWebPro <betopone1021@gmail.com>
2018-10-06 02:33:49 +0300

    update

--- www/handpoint.js
+++ www/handpoint.js
@@ -1,5 +1,6 @@
 cordova.define("com.cordova.handpointplugin.HandPointPlugin", function(require, exports, module) { /*global cordova*/
-module.exports = {
+
+	module.exports = {
 
     connect: function (name, address, port, method, success, failure) {
         cordova.exec(success, failure, "HandPointPlugin", "connect", [{"name": name, "address": address, "port": port, "method": method}]);
